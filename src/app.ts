bc07fa77a54deab0d90494d175a17bbe556dad2b
chenrj123 <729921697@qq.com>
2024-11-28 18:46:47 +0800

    add

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());  // 解析 JSON 请求体
 app.use('/', router);  // 注册任务相关路由
 import RpcConnectionPool  from './services/RpcConnectionPool';
-const rpcPool = new RpcConnectionPool(5, 'testnet-10'); // 连接池大小为 5 mainnet
+const rpcPool = new RpcConnectionPool(5, 'mainnet'); // 连接池大小为 5
 export default rpcPool;
 app.listen(3000, async () => {
     console.log('Server running on port 3000');
