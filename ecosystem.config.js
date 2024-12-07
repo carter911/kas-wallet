3bd50fe1ea8d4fbbe24a3d9340fb423266191d7c
chenrj123 <729921697@qq.com>
2024-12-07 11:42:48 +0800

    update deploy.sh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
             script: "./dist/app.js", // 启动脚本路径
             instances: "max", // 启动实例数量，"max" 表示根据 CPU 核心数自动设置
             exec_mode: "cluster", // 使用 cluster 模式（推荐多实例时使用）
-            watch: true, // 是否监听文件变化（适合开发环境）
+            watch: false, // 是否监听文件变化（适合开发环境）
             ignore_watch: ["node_modules", "logs"], // 忽略监听的文件或目录
             max_memory_restart: "600M", // 内存占用超过 300MB 时重启
             env: {
